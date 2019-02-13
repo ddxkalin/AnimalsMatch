ef26819fdffe96ba9d39c5f7556e268c2f743f33
kalinstoev <kalinstoev@fan.gov>
2019-02-13 15:35:15 +0200

    Added Base Controller

--- Pets/Pets.Web/Controllers/HomeController.cs
+++ Pets/Pets.Web/Controllers/HomeController.cs
@@ -22,4 +22,36 @@
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+
+    //    public IActionResult Index()
+    //    {
+    //    var client = new RestClient("https://api.predicthq.com/v1/events/");
+    //    client.AddDefaultHeader("Authorization", "Bearer ZN79Mw9lMMWGgNMDSA3NwO5MpoczZI");
+    //    client.AddDefaultHeader("Accept", "application/json");
+    //    var restRequest = new RestRequest();
+    //    restRequest.AddQueryParameter("category", "festivals");
+    //    restRequest.AddQueryParameter("label", "movie");
+    //    restRequest.AddQueryParameter("q", "film");
+    //    restRequest.AddQueryParameter("offset", "4");
+    //    var response = client.Get(restRequest);
+    //    var movieFestivalsList = new List<MovieFestival>();
+
+    //        try
+    //    {
+    //        if (response.IsSuccessful)
+    //        {
+    //            var results = JsonConvert.DeserializeObject<MovieFestivalsResult>(response.Content);
+    //            movieFestivalsList = results.Results;
+    //        }
+    //    }
+    //    catch
+    //    {
+    //    }
+
+    //    return this.View(movieFestivalsList);
+    //}
+
+    //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    //public IActionResult Error() => this.View();
+    //}
 }
