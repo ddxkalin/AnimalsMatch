fcbaa4b943ae8a6318327f15cfff0d314832a3ba
kalinstoev <kalinstoev@fan.gov>
2019-02-13 13:38:47 +0200

    Added app SignIn and User Manager (error at SignInManager?!)

---
+++ Pets/Pets.Services/Identity/ApplicationSignInManager.cs
@@ -0,0 +1,41 @@
+﻿namespace Pets.Services
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
+    using Pets.Data.Models;
+    using Pets.Services.Identity;
+    using Microsoft.AspNetCore.Authentication;
+
+    public class ApplicationSignInManager<TUser> : SignInManager<ApplicationUser>
+        where TUser : ApplicationUser
+    {
+        public ApplicationSignInManager(
+            ApplicationUserManager<ApplicationUser> userManager,
+            IHttpContextAccessor contextAccessor,
+            IUserClaimsPrincipalFactory<ApplicationUser> claimsFactory,
+            IOptions<IdentityOptions> optionsAccessor,
+            ILogger<SignInManager<ApplicationUser>> logger,
+            IAuthenticationSchemeProvider schemeProvider
+        )
+        // Todo: can't take 6 arguments?
+        : base(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemeProvider)
+        {
+        }
+
+        public override async Task<SignInResult> PasswordSignInAsync(string userName, string password, bool rememberMe, bool lockoutOnFailure)
+        {
+            var user = await this.UserManager.FindByEmailAsync(userName);
+
+            if (user == null || !user.IsActive || user.IsDeleted)
+            {
+                return SignInResult.NotAllowed;
+            }
+
+            return await this.PasswordSignInAsync(user, password, rememberMe, lockoutOnFailure: lockoutOnFailure);
+        }
+    }
+}
---
+++ Pets/Pets.Services/Identity/ApplicationUserManager.cs
@@ -0,0 +1,144 @@
+﻿namespace Pets.Services.Identity
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
+    using Pets.Data.Models;
+
+    public class ApplicationUserManager<TUser> : UserManager<ApplicationUser>
+        where TUser : ApplicationUser
+    {
+        public ApplicationUserManager(
+            IUserStore<ApplicationUser> store,
+            IOptions<IdentityOptions> optionsAccessor,
+            IPasswordHasher<ApplicationUser> passwordHasher,
+            IEnumerable<IUserValidator<ApplicationUser>> userValidators,
+            IEnumerable<IPasswordValidator<ApplicationUser>> passwordValidators,
+            ILookupNormalizer keyNormalizer,
+            IdentityErrorDescriber errors,
+            IServiceProvider services,
+            ILogger<UserManager<ApplicationUser>> logger)
+            : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors,
+                services, logger)
+        {
+        }
+
+        public async Task<IdentityResult> ActivateUserAsync(string userId)
+        {
+            if (userId == null)
+            {
+                throw new InvalidOperationException("userId");
+            }
+
+            ApplicationUser user = await this.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                throw new InvalidOperationException("user");
+            }
+
+            user.IsActive = true;
+
+            return await this.UpdateAsync(user);
+        }
+
+        public async Task<IdentityResult> DeactivateUserAsync(string userId)
+        {
+            if (userId == null)
+            {
+                throw new InvalidOperationException("userId");
+            }
+
+            ApplicationUser user = await this.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                throw new InvalidOperationException("user");
+            }
+
+            user.IsActive = false;
+
+            return await this.UpdateAsync(user);
+        }
+
+        public async Task<IdentityResult> DeleteUserAsync(string userId)
+        {
+            if (userId == null)
+            {
+                throw new InvalidOperationException("userId");
+            }
+
+            ApplicationUser user = await this.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                throw new InvalidOperationException("user");
+            }
+
+            user.IsDeleted = true;
+
+            return await this.UpdateAsync(user);
+        }
+
+        public async Task<IdentityResult> RestoreUserAsync(string userId)
+        {
+            if (userId == null)
+            {
+                throw new InvalidOperationException("userId");
+            }
+
+            ApplicationUser user = await this.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                throw new InvalidOperationException("user");
+            }
+
+            user.IsDeleted = false;
+
+            return await this.UpdateAsync(user);
+        }
+
+        public async Task<IdentityResult> UnlockUserAsync(string userId)
+        {
+            if (userId == null)
+            {
+                throw new InvalidOperationException("userId");
+            }
+
+            ApplicationUser user = await this.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                throw new InvalidOperationException("user");
+            }
+
+            user.LockoutEnd = DateTime.UtcNow;
+
+            return await this.UpdateAsync(user);
+        }
+
+        public async Task<IdentityResult> SetEmailConfirmationTokenResentOnAsync(string userId)
+        {
+            if (userId == null)
+            {
+                throw new InvalidOperationException("userId");
+            }
+
+            ApplicationUser user = await this.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                throw new InvalidOperationException("user");
+            }
+
+            user.EmailConfirmationTokenResentOn = DateTime.UtcNow;
+
+            return await this.UpdateAsync(user);
+        }
+    }
+}
