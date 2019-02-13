de229cbbbcab0f708086fca9a22fe5af230b47ca
kalinstoev <kalinstoev@fan.gov>
2019-02-13 15:38:27 +0200

    Added View Models

---
+++ Pets/Pets.Web/Models/AlertVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Pets.Web.Models
+{
+    public class AlertVM
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+    }
+}
--- Pets/Pets.Web/Program.cs
+++ Pets/Pets.Web/Program.cs
@@ -3,14 +3,14 @@
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
 
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
     }
