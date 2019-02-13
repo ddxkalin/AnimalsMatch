2e41c69799ec3d37d46066f27767e8437b280540
kalinstoev <kalinstoev@fan.gov>
2019-02-13 13:37:57 +0200

    Added Pets.Common for GlobalConstants, Settings and Automapper

---
+++ Pets/Pets.Common/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace Pets.Common.Exceptions
+{
+    using System;
+
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ Pets/Pets.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Pets.Common
+{
+    public static class GlobalConstants
+    {
+        public const string AdministratorRoleName = "Administrator";
+
+        public const string UserRoleName = "User";
+    }
+}
