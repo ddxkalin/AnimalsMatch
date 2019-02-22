267778175141543c9040313df04d5de3828473ec
kalinstoev <kalinstoev@fan.gov>
2019-02-22 12:53:10 +0200

    Added PetsDbContextSeeder

---
+++ Pets/Pets.Data/Seeding/PetsDbContextSeeder.cs
@@ -0,0 +1,100 @@
+﻿using Pets.Common;
+using Pets.Data.Models;
+
+namespace Pets.Data.Seeding
+{
+    using System;
+    using System.Linq;
+   using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class PetsDbContextSeeder
+    {
+        public static void Seed(PetsDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException(nameof(dbContext));
+            }
+
+            if (serviceProvider == null)
+            {
+                throw new ArgumentNullException(nameof(serviceProvider));
+            }
+
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            Seed(dbContext, userManager, roleManager);
+        }
+
+        public static void Seed(PetsDbContext dbContext, UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException(nameof(dbContext));
+            }
+
+            if (userManager == null)
+            {
+                throw new ArgumentNullException(nameof(userManager));
+            }
+
+            if (roleManager == null)
+            {
+                throw new ArgumentNullException(nameof(roleManager));
+            }
+
+            SeedRoles(roleManager);
+            //SeedAdmin(userManager);
+        }
+
+        private static void SeedAdmin(UserManager<ApplicationUser> userManager)
+        {
+            ApplicationUser admin = new ApplicationUser
+            {
+                Firstname = "System",
+                Lastname = "Administrator",
+                Email = "admin@admin.com",
+                UserName = "admin@admin.com",
+                EmailConfirmed = true,
+                IsActive = true,
+                CreatedOn = DateTime.Now,
+                ModifiedOn = DateTime.Now,
+            };
+
+            var result = userManager.CreateAsync(admin, "123456").GetAwaiter().GetResult();
+
+            if (!result.Succeeded)
+            {
+                throw new Exception(string.Join(Environment.NewLine, result.Errors.Select(e => e.Description)));
+            }
+
+            var roleResult = userManager.AddToRoleAsync(admin, GlobalConstants.AdministratorRoleName).GetAwaiter().GetResult();
+
+            if (!roleResult.Succeeded)
+            {
+                throw new Exception(string.Join(Environment.NewLine, roleResult.Errors.Select(e => e.Description)));
+            }
+        }
+
+        private static void SeedRoles(RoleManager<ApplicationRole> roleManager)
+        {
+            SeedRole(GlobalConstants.AdministratorRoleName, roleManager);
+            SeedRole(GlobalConstants.UserRoleName, roleManager);
+        }
+
+        private static void SeedRole(string roleName, RoleManager<ApplicationRole> roleManager)
+        {
+            var role = roleManager.FindByNameAsync(roleName).GetAwaiter().GetResult();
+            if (role == null)
+            {
+                var result = roleManager.CreateAsync(new ApplicationRole(roleName)).GetAwaiter().GetResult();
+
+                if (!result.Succeeded)
+                {
+                    throw new Exception(string.Join(Environment.NewLine, result.Errors.Select(e => e.Description)));
+                }
+            }
+        }
+    }
+}
