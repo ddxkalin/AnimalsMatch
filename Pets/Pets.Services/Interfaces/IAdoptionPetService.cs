aa90f8e53fa5c3832d9c24fb68305018326e8ff7
kalinstoev <kalinstoev@fan.gov>
2019-02-22 13:33:29 +0200

    Bug Fix at IAdoptionPetService

--- Pets/Pets.Services/Interfaces/IAdoptionPetService.cs
+++ Pets/Pets.Services/Interfaces/IAdoptionPetService.cs
@@ -2,8 +2,9 @@
 {
     using Models;
     using System.Collections.Generic;
+    using Pets.Data.Common.Models;
 
-    public interface IAdoptionPetService<T>
+    public interface IAdoptionPetService<T> where T : BaseDeletableModel<string>
     {
         IEnumerable<AdoptionPetListingServiceModel> Requested();
         void Adopt(int id, string username);
